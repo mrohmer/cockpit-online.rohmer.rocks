3d892879416946acee1cc94fc8d4e443a43d0bf8
mrohmer <rohmer.matze@gmail.com>
2022-06-23 16:18:38 +0200

    fix: fix links

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -19,13 +19,13 @@
 
 <Content class="mt-2">
 	<div class="text-center text-xs opacity-70">
-		Daten bereitgestellt von <a href="https://online.cockpit-xp.de" target="_blank" rel="noopener nofollow" class="text-primary">online.cockpit-xp.de</a>.<br>
+		Daten bereitgestellt von <a href="https://online.cockpit-xp.de" target="_blank" rel="noreferrer noopener" class="text-primary">online.cockpit-xp.de</a>.<br>
 		Dies ist eine Überarbeitung des UIs für private Zwecke.
 	</div>
 	<div class="-mt-12">
 		<PoweredBy name="Matthias Rohmer"
 				   url="https://matthias.rohmer.rocks"
 				   technologies={['Cockpit-XP Online', 'svelte', 'netlify']}
-				   sourceCodeUrl="https://github.com/mrohmer/haekeln.rohmer.rocks" />
+				   sourceCodeUrl="https://github.com/mrohmer/carrera-live.rohmer.rocks" />
 	</div>
 </Content>
