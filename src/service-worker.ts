213aae2a889230a61b7d39a0e9a7609b0fd2505c
mrohmer <rohmer.matze@gmail.com>
2022-07-22 20:25:26 +0200

    feat: add offline capabilities

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,19 +1,46 @@
 import { build, files } from '$service-worker';
-import {registerRoute} from 'workbox-routing';
+import {registerRoute, setDefaultHandler, setCatchHandler} from 'workbox-routing';
 import {NetworkFirst, CacheFirst} from 'workbox-strategies';
+import {warmStrategyCache} from 'workbox-recipes';
 
+const INDEX_URL = '/';
+const FALLBACK_SESSION_URL = '/session';
+const FALLBACK_SESSION_DETAIL_URL = '/session/slot/0';
 const urls = [
-  '/',
+  INDEX_URL,
+  FALLBACK_SESSION_URL,
+  FALLBACK_SESSION_DETAIL_URL,
   ...build,
   ...files,
 ];
-registerRoute(
-  ({url}) => urls.includes(url.pathname),
-  new NetworkFirst(),
-);
+const strategy = new NetworkFirst();
+warmStrategyCache({
+  urls,
+  strategy,
+});
+
 registerRoute(
   ({url}) => /\/api\/[\w\/]*\/image\/[1-8]\/(driver|car).(jpe?g|png|svg|gif)/gi.test(url.pathname),
   new CacheFirst(),
 );
+
+setDefaultHandler(strategy);
+
+setCatchHandler(async ({request, event}) => {
+  if (request.destination === 'document') {
+    const url = new URL(request.url);
+
+    if (/^(\/)?(\?|#|$)/.test(url.pathname)) {
+      return strategy.handle({event, request: INDEX_URL});
+    }
+    if (/^(\/\w+)+\/slot\/\d+(\?|#|$)/.test(url.pathname)) {
+      return strategy.handle({event, request: FALLBACK_SESSION_DETAIL_URL});
+    }
+    if (/^(\/\w+)+(\?|#|$)/.test(url.pathname)) {
+      return strategy.handle({event, request: FALLBACK_SESSION_URL});
+    }
+  }
+  return Response.error();
+});
 
 skipWaiting();
