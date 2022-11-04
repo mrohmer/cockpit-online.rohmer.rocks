264fb58222833402897f1460280e008242790e6c
mrohmer <rohmer.matze@gmail.com>
2022-11-04 20:27:44 +0100

    fix: add fullscreen

--- src/lib/components/SessionHeader.svelte
+++ src/lib/components/SessionHeader.svelte
@@ -1,17 +1,42 @@
 <script lang="ts">
-    import type {Race} from '../models/race';
-    import {createEventDispatcher} from 'svelte';
-    import IoIosArrowBack from 'svelte-icons/io/IoIosArrowBack.svelte';
-    import {slide} from 'svelte/transition';
+  import type {Race} from '../models/race';
+  import {createEventDispatcher, onMount} from 'svelte';
+  import IoIosArrowBack from 'svelte-icons/io/IoIosArrowBack.svelte';
+  import IoIosExpand from 'svelte-icons/io/IoIosExpand.svelte';
+  import IoIosContract from 'svelte-icons/io/IoIosContract.svelte';
+  import {slide} from 'svelte/transition';
 
-    const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-    export let status: Race['status'];
-    export let name: Race['name'];
-    export let mode: Race['mode'];
-    export let time: Race['time'];
-    export let lapsToGo: Race['lapsToGo'];
-    export let backLink = '/';
+  let canGoFullscreen = false;
+  let isFullscreen = false;
+  const toggleFullscreen = () => {
+    if (isFullscreen) {
+        document.exitFullscreen();
+    } else {
+      document.body.requestFullscreen();
+    }
+  }
+
+  const onFullscreenChange = () => {
+    isFullscreen = !!document.fullscreenElement;
+  }
+
+  onMount(() => {
+    canGoFullscreen = document.fullscreenEnabled;
+    onFullscreenChange();
+
+    window.addEventListener('fullscreenchange', onFullscreenChange);
+
+    return () => window.removeEventListener('fullscreenchange', onFullscreenChange)
+  })
+
+  export let status: Race['status'];
+  export let name: Race['name'];
+  export let mode: Race['mode'];
+  export let time: Race['time'];
+  export let lapsToGo: Race['lapsToGo'];
+  export let backLink = '/';
 </script>
 
 <div class="mt-4 mb-10">
@@ -20,10 +45,22 @@
             <IoIosArrowBack/>
         </a>
         <div class="flex-1 min-w-0">
-            <h1 class="text-3xl text-center pr-10 overflow-hidden text-ellipsis whitespace-nowrap">
+            <h1 class="text-3xl text-center overflow-hidden text-ellipsis whitespace-nowrap"
+                class:pr-10={!canGoFullscreen}>
                 {name}
             </h1>
         </div>
+        {#if canGoFullscreen}
+            <div class="w-10 h-6 px-2 cursor-pointer"
+                 title="Fullscreen"
+                 on:click={toggleFullscreen}>
+                {#if isFullscreen}
+                    <IoIosContract/>
+                {:else}
+                    <IoIosExpand/>
+                {/if}
+            </div>
+        {/if}
     </div>
 
     {#if mode}
