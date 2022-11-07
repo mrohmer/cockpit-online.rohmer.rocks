da2f65ffbd6b7a9a6058d1272e68cb92df91d8bd
Matthias Rohmer <rohmer.matze@gmail.com>
2022-11-07 23:50:02 +0100

    fix: remove prerendering config

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,5 @@
 import type {PageLoad} from './$types';
 
-export const prerender = true;
-
 export const load: PageLoad = async ({fetch}) => {
   const response = await fetch('/api/sessions/public');
   const publicSessions = await response.json();
