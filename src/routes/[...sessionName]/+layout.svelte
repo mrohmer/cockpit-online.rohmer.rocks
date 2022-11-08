5277cb81178705ac7bf015cda50603ede67a9b59
mrohmer <rohmer.matze@gmail.com>
2022-11-08 09:10:40 +0100

    fix(session): fix latest session persistence in error cases

--- src/routes/[...sessionName]/+layout.svelte
+++ src/routes/[...sessionName]/+layout.svelte
@@ -5,11 +5,12 @@
   import type {Race} from '$lib/models/race';
   import { page } from '$app/stores';
   import {cleanSessionName} from "$lib/utils/clean-session-name.js";
-  import {addSession} from '../../lib/utils/sessions';
+  import {addSession, removeSession} from '$lib/utils/sessions';
+  import {browser} from '$app/environment';
 
   export let data: Race;
 
-  $: data && $page.params.sessionName && addSession($page.params.sessionName);
+  $: browser && data && $page.params.sessionName && (data?.slots?.length ? addSession($page.params.sessionName) : removeSession($page.params.sessionName));
 </script>
 <Content>
     <SessionHeader {...data} backLink={$page.route.id === "/[...sessionName]/slot/[slotId]" ? `/${cleanSessionName($page.params.sessionName)}` : '/'} on:clickBackLink={handleBackLinkClick} />
